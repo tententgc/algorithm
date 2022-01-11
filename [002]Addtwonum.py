92bd2c08b66f3b99fb5244693619ed631aea128f
tententgc <nyman3527@gmail.com>
2022-01-11 16:26:00 +0700

    Update [002]Addtwonum.py

--- [002]Addtwonum.py
+++ [002]Addtwonum.py
@@ -1,16 +1,26 @@
-from typing import Optional
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        i = 0
+        a = 0
+        b = 0
+        while l1:
+            a = a+l1.val*(10**i)
+            l1 = l1.next
+            i = i+1
+        i = 0
+        while l2:
+            b = b+l2.val*(10**i)
+            l2 = l2.next
+            i = i+1
+        c = a+b
 
+        d = [int(i) for i in str(c)]
+        d.reverse()
 
-class Solution:
-        def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-            sum1=''
-            sum2=''
-            for i in l1:
-                sum1+=i 
-            for i in l2:
-                sum2+=i
-            sumall = int(sum1)+int(sum2)
-            for i in sumall:
-                Optional.append(i)
-            result.reverse() 
-            return result
+        head = ListNode(d[0])
+        current = head
+        for i in d[1:]:
+            current.next = ListNode(i)
+            current = current.next
+
+        return head
