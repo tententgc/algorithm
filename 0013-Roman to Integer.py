182e98eeb24b39103ed4d33e11377f5e0c1d6c82
tententgc <nyman3527@gmail.com>
2022-01-20 09:16:24 +0700

    Create 0013-Roman to Integer.py

---
+++ 0013-Roman to Integer.py
@@ -0,0 +1,12 @@
+def romanTOInt(s): 
+    case = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    solution = case[s[-1]]
+    if len(s) > 1 :
+        for idx in range (len(s)-2 ,-1 ,-1):
+            case[s[idx]]
+            if case[s[idx]] < case[s[idx+1]]: 
+                solution -= case[s[idx]] 
+            else:solution += case[s[idx]] 
+    return solution 
+a= input()
+romanTOInt(a)
