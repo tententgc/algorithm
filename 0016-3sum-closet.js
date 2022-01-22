49a33cb2bbd0e1068b43e1e2bb2b6cf7d13eaa2c
tententgc <nyman3527@gmail.com>
2022-01-23 01:11:11 +0700

    Create 0016-3sum-closet.js

---
+++ 0016-3sum-closet.js
@@ -0,0 +1,24 @@
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b)
+    let diff = Infinity;
+
+    for (var i = 0; i < nums.length; i++) {
+        let j = i + 1;
+        let k = nums.length - 1
+
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k]
+
+            if (Math.abs(diff) > Math.abs(target - sum)) {
+                diff = target - sum
+            }
+
+            if (sum < target) {
+                j++
+            } else {
+                k--
+            }
+        }
+    }
+    return target - diff
+};
