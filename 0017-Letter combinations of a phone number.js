e6af920ccabc2edc1c0ec114da5839d231dd68b1
tententgc <nyman3527@gmail.com>
2022-01-24 20:44:06 +0700

    Create 0017-Letter combinations of a phone number.js

---
+++ 0017-Letter combinations of a phone number.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+    const mapping = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+    }
+    digits = digits.split("")
+    let result = []
+    for (const digit of digits) {
+        result = charStringMutiply(mapping[digit], result)
+    }
+    return result
+};
+
+var charStringMutiply = function (chars, strings) {
+    const result = []
+    if (strings.length == 0) {
+        return chars
+    }
+    for (const character of chars) {
+        for (const st of strings) {
+            result.push(st + character)
+        }
+    }
+    return result
+}
