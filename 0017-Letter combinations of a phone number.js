530d4d65d3238406d69e992d8bc1d2492a91a801
tententgc <nyman3527@gmail.com>
2022-02-02 18:12:29 +0700

    Update 0017-Letter combinations of a phone number.js

--- 0017-Letter combinations of a phone number.js
+++ 0017-Letter combinations of a phone number.js
@@ -1,3 +1,19 @@
+/* Given a string of digits, return all possible letter combinations that the number could represent.
+
+The mapping of digits to letters is:
+
+2 -> a, b, c
+3 -> d, e, f
+4 -> g, h, i
+5 -> j, k, l
+6 -> m, n, o
+7 -> p, q, r, s
+8 -> t, u, v
+9 -> w, x, y, z
+
+The mapping is provided in the form of a hash table.
+
+The function takes two parameters */
 /**
  * @param {string} digits
  * @return {string[]}
