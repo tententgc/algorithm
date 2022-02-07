e496183aab9e1b9882740f20bfcacbc6d1ccd735
tententgc <nyman3527@gmail.com>
2022-02-07 22:06:30 +0700

    Update 0001-Two_sum.py

--- 0001-Two_sum.py
+++ 0001-Two_sum.py
@@ -1,3 +1,14 @@
+# For each element in the list, check if the target - element exists in the rest of the list. 
+# 
+# If it does, return the index of the element and the index of the target - element. 
+# 
+# If it doesn't, continue to the next element. 
+# 
+# If you've gone through all the elements and haven't found a match, return an empty list.
+# 
+# # Python
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
