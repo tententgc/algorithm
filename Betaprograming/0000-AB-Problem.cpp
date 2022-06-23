6cb87164ac3d1d07a17d364f18149f5ab290a4fe
tententgc <nyman3527@gmail.com>
2022-06-24 02:53:48 +0700

    �  config beta file count.yml

---
+++ Betaprograming/0000-AB-Problem.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    int input, sum;
+    scanf("%d", &input);
+    scanf("%d", &sum);
+    sum = sum + input;
+    printf("%d", sum);
+    return 0;
+    /*long  aa,a,b;
+    scanf("%ld",&a);
+    scanf("%ld",&b);
+    aa=a+b;
+    printf("%ld",aa);*/
+}
--- Betaprograming/beta-count.py
+++ Betaprograming/beta-count.py
@@ -15,7 +15,7 @@ class GithubController:
 	def __init__(self, ACCESS_TOKEN):
 		g = github.Github(ACCESS_TOKEN)
 		self.repo = g.get_repo('tententgc/LeetCodeSolution')
-		self.PATH = 'BetaPrograming/beta-count.md'
+		self.PATH = 'Betaprograming/beta-count.md'
 
 	def get_data(self):
 		contents = self.repo.get_contents(self.PATH, 'main')
