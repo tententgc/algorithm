4221ba58943fbe2b62ab3b8c3586fa4592f81302
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-25 16:47:29 +0700

    Update 0001-Two_sum.py

--- Leetcode/0001-Two_sum.py
+++ Leetcode/0001-Two_sum.py
@@ -1,18 +1,19 @@
-# For each element in the list, check if the target - element exists in the rest of the list. 
-# 
-# If it does, return the index of the element and the index of the target - element. 
-# 
-# If it doesn't, continue to the next element. 
-# 
-# If you've gone through all the elements and haven't found a match, return an empty list.
-# 
-# # Python
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        for i in range(len(nums)): 
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+nums = [2, 7, 11, 15]
+target = 9
+def twosum(nums, target) :
+    dic = dict()
+
+    for val, key in enumerate(nums):
+        if key not in dic:
+            dic[key] = [val]
+        else:
+            dic[key].append(val)
+
+    for key, val in dic.items():
+        if target-key in dic:
+            if key != target-key:
+                return [dic[key][0], dic[target-key][0]]
+            elif len(dic[key])  > 1:
+                return [dic[key][0], dic[key][1]]
+
+print(twosum(nums, target)) 
